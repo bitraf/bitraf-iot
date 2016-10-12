2771bd407cdb6c0c270ecc7af55f11773b9e406f
Jon Nordby <jononor@gmail.com>
2016-10-12 22:04:41 +0200

    GuestLogin: Add initial code

---
+++ participants/GuestLogin.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys, os, json, logging
+sys.path.append(os.path.abspath("."))
+
+import gevent
+import msgflo
+
+class Repeat(msgflo.Participant):
+  def __init__(self, role):
+    d = {
+      'component': 'GuestLogin',
+      'label': 'Trigger door opening from using button',
+      'inports': [
+        { 'id': 'in', 'type': 'bang', 'queue': '/bitraf/button/1/event' },
+      ],
+      'outports': [
+        { 'id': 'openfront', 'type': 'int', 'queue': '/bitraf/door/frontdoor/open' },
+        { 'id': 'opendownstairs', 'type': 'int', 'queue': '/bitraf/door/2floor/open' },
+      ],
+    }
+    msgflo.Participant.__init__(self, d, role)
+
+  def process(self, inport, msg):
+    print 'received', inport, msg
+
+    frontdoor_time = 10 # seconds
+    innerdoor_time = 60 # seconds
+
+    self.send('openfront', frontdoor_time)
+    self.send('opendownstairs', innerdoor_time)
+
+    self.ack(msg)
