91a08f4130fb04e5735daaec9e049a2f537c733a
Jon Nordby <jononor@gmail.com>
2017-06-27 16:31:59 +0200

    singlebutton: Adapt config

---
+++ singlebutton/config.h
@@ -0,0 +1,6 @@
+// Replace these with the appropriate values
+#define CFG_WIFI_SSID "bitraf24"
+#define CFG_WIFI_PASSWORD "grimbadgerassault"
+#define CFG_MQTT_HOST "mqtt.bitraf.no"
+#define CFG_MQTT_USER "bitraf-door"
+#define CFG_MQTT_PASSWORD "The7rohrah"
--- singlebutton/singlebutton.ino
+++ singlebutton/singlebutton.ino
@@ -6,23 +6,25 @@
 #include <Msgflo.h>
 #include <PubSubClient.h>
 
+#include "./config.h"
+
 struct Config {
   const int ledPin = 14;
   const int buttonPin = 12;
-  const String role = "button";
+  const String role = "bitraf/guestbutton/lab";
 
-  const String wifiSsid = "jhaven";
-  const String wifiPassword = "abcdefgh";
+  const String wifiSsid = CFG_WIFI_SSID;
+  const String wifiPassword = CFG_WIFI_PASSWORD;
 
-  const char *mqttHost = "192.168.43.85";
+  const char *mqttHost = CFG_MQTT_HOST;
   const int mqttPort = 1883;
 
-  const char *mqttUsername = "myuser";
-  const char *mqttPassword = "mypassword";
+  const char *mqttUsername = CFG_MQTT_USER;
+  const char *mqttPassword = CFG_MQTT_PASSWORD;
 } cfg;
 
 
-const auto participant = msgflo::Participant("fosdem2017/Relay", cfg.role);
+const auto participant = msgflo::Participant("bitraf/LedButton", cfg.role);
 
 WiFiClient wifiClient; // used by WiFi
 PubSubClient mqttClient;
