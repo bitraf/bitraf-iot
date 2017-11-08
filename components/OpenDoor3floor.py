b73ace7604b1670b192755aa7f5e981b220009d7
Torfinn Ingolfsen <tingox@gmail.com>
2017-11-08 15:11:17 +0100

    code to open the 3rd floor doors when the button in the workshop or the 3rd floor office is pressed

---
+++ components/OpenDoor3floor.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys, os, json, logging
+sys.path.append(os.path.abspath("."))
+
+import gevent
+import msgflo
+
+class Repeat(msgflo.Participant):
+  def __init__(self, role):
+    self.current_input = False
+
+    d = {
+      'component': 'OpenDoor3floor',
+      'label': 'Trigger door opening from using button',
+      'inports': [
+        { 'id': 'in', 'type': 'boolean', 'queue': 'bitraf/openbutton/workshop/button' },
+        { 'id': 'in', 'type': 'boolean', 'queue': 'bitraf/openbutton/office3rdfloor/button' },        
+      ],
+      'outports': [
+        { 'id': 'open3floor', 'type': 'int', 'queue': '/bitraf/door/3floor/open' },
+      ],
+    }
+    msgflo.Participant.__init__(self, d, role)
+
+  def process(self, inport, msg):
+    print 'received', inport, msg.data
+
+    trigger = msg.data == True and not self.current_input
+    if trigger:
+        door_time = 20 # seconds
+        self.send('open3floor', door_time)
+        print 'tried to open doors'
+
+    self.current_input = msg.data
+    self.ack(msg)
